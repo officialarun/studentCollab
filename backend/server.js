4fd132dc4ea7c74d836293b155e512969cbdbae9
officialarun <officialarunpandey0600@gmail.com>
2025-04-23 03:31:24 +0530

    sameSite rule to none

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use(session({
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000, // 1 day
-        sameSite: 'lax'
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     }
 }));
 
