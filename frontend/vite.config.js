d5479f22eb442c52acbf3d76d714575e99695bf2
officialarun <officialarunpandey0600@gmail.com>
2025-04-22 21:36:48 +0530

    adding all changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://codespace-4bbx.onrender.com/api',
         changeOrigin: true,
         secure: false
       }
